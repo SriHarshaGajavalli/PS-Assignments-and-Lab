5d14dfe4ee3ef042c69c8bd15176dbf39503b189
SriHarshaGajavalli <sriharsha.g15@iiits.in>
2018-01-21 17:15:34 +0530

    R Prgrm

---
+++ Assignment1/refprblm/1.R
@@ -0,0 +1,14 @@
+people <- list("Vaishnav", "Sri", "Harsha", "Vaishali", "Abc")
+j <- 1
+
+
+for(i in 1:length(people)) {
+  if(nchar(people[i]) > 5) {
+    result[j] <- people[i]
+    j = j+1
+  }
+}
+
+library(dplyr)
+result <- mutate_all(result, funs=toupper)
+print(result)
